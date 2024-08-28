824b6fb899cd72d9f17c332f919fd4893b23717c
ike-nanino <ikesemanu@gmail.com>
2024-08-28 13:55:56 -0400

    minor fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import GenreDropDown from './GenreDropDown'
 
 function Header() {
   return (
-    <header className='fixed w-full z-10 top-0 flex items-center justify-between p-10 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900 '>
+    <header className='fixed w-full z-[15] top-0 flex items-center justify-between p-10 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900 '>
         <div className='flex items-center'>
             <Link href="/" className='mr-10'>
             <h1 className='cursor-pointer text-2xl font-bold hover:text-gray-300 transition ease-in-out delay-250 '>STREAMVERSE</h1>
