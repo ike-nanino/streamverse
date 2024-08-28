18b2c0d2c0cb0a871e0aac4d5ef7c2b09e64c401
ike-nanino <ikesemanu@gmail.com>
2024-08-28 13:58:43 -0400

    viewport changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { League_Spartan } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import type { Viewport } from 'next'
 
 const spartan = League_Spartan({ subsets: ["latin"] });
 
@@ -10,6 +11,14 @@ export const metadata: Metadata = {
   title: "Streamverse",
   description: "A movie streaming platform by Nanino",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  height: 'device-height',
+  initialScale: 1,
+  userScalable: false,
+
+}
 
 export default function RootLayout({
   children,
